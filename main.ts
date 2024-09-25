5ec700f1ada06973e6d4ceb9cdf966a3fa8563ff
itsryu <joaovictor9162@outlook.com>
2024-09-25 12:40:40 -0300

    fix: arrumando dados que estavam com string vazias para NULL

--- main.ts
+++ main.ts
@@ -23,12 +23,17 @@ export class SQLConstructor {
         const requiredFields = new Set(['ID', 'rodata', 'data', 'hora', 'mandante', 'visitante', 'formacao_mandante', 'formacao_visitante', 'tecnico_mandante', 'tecnico_visitante', 'vencedor', 'arena', 'mandante_Placar', 'visitante_Placar', 'mandante_Estado', 'visitante_Estado']);
 
         let sql = `SET GLOBAL net_buffer_length = 1000000;\nSET GLOBAL max_allowed_packet = 1000000000;\n\nUSE brasileirao;\n\n`;
-        
+
         sql += `INSERT INTO partidas (id_partida, rodada, horario, data_partida, mandante, visitante, formacao_mandante, formacao_visitante, tecnico_mandante, tecnico_visitante, vencedor, arena, mandante_placar, visitante_placar, estado_mandante, estado_visitante)\nVALUES\n` +
             rows.reduce<string[]>((acc, row) => {
                 if ([...requiredFields].every(field => (row)[field] !== undefined)) {
                     if (row.data) row.data = row.data.split('/').reverse().join('-');
-                    acc.push(`\t(${row.ID}, ${row.rodata}, "${row.hora}", "${row.data}", "${row.mandante}", "${row.visitante}", "${row.formacao_mandante}", "${row.formacao_visitante}", "${row.tecnico_mandante}", "${row.tecnico_visitante}", "${row.vencedor}", "${row.arena}", ${row.mandante_Placar}, ${row.visitante_Placar}, "${row.mandante_Estado}", "${row.visitante_Estado}")`);
+                    row.formacao_mandante = !row.formacao_mandante ? 'NULL' : `"${row.formacao_mandante}"`;
+                    row.formacao_visitante = !row.formacao_visitante ? 'NULL' : `"${row.formacao_visitante}"`;
+                    row.tecnico_mandante = !row.tecnico_mandante ? 'NULL' : `"${row.tecnico_mandante}"`;
+                    row.tecnico_visitante = !row.tecnico_visitante ? 'NULL' : `"${row.tecnico_visitante}"`;
+
+                    acc.push(`\t(${row.ID}, ${row.rodata}, "${row.hora}", "${row.data}", "${row.mandante}", "${row.visitante}", ${row.formacao_mandante}, ${row.formacao_visitante}, ${row.tecnico_mandante}, ${row.tecnico_visitante}, "${row.vencedor}", "${row.arena}", ${row.mandante_Placar}, ${row.visitante_Placar}, "${row.mandante_Estado}", "${row.visitante_Estado}")`);
                 }
                 return acc;
             }, []).join(',\n') + ';';
@@ -48,7 +53,9 @@ export class SQLConstructor {
         sql += `INSERT INTO gols (id_gol, id_partida, rodada, clube, atleta, minuto, tipo_de_gol)\nVALUES\n` +
             rows.reduce<string[]>((acc, row) => {
                 if ([...requiredFields].every(field => (row)[field] !== undefined)) {
-                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", "${row.atleta}", "${row.minuto}", "${row.tipo_de_gol}")`);
+                    row.tipo_de_gol = !row.tipo_de_gol ? 'NULL' : `"${row.tipo_de_gol}"`;
+
+                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", "${row.atleta}", "${row.minuto}", ${row.tipo_de_gol})`);
                 }
                 return acc;
             }, []).join(',\n') + ';';
@@ -61,15 +68,17 @@ export class SQLConstructor {
         const csvPath = resolve('./data/campeonato-brasileiro-cartoes.csv');
         const content = readFileSync(csvPath, { encoding: 'utf-8' });
         const rows = SQLConstructor.parseCSV<cartoesData>(content);
-        const requiredFields = new Set(['partida_id', 'rodata', 'clube', 'cartao', 'atleta', 'num_camisa', 'posicao', 'minuto']);
+        const requiredFields = new Set(['partida_id', 'rodata', 'clube', 'cartao', 'atleta', 'minuto']);
 
         let sql = `SET GLOBAL net_buffer_length = 1000000;\nSET GLOBAL max_allowed_packet = 1000000000;\n\nUSE brasileirao;\n\n`;
 
         sql += `INSERT INTO cartoes (id_cartao, id_partida, rodada, clube, cartao, atleta, num_camisa, posicao, minuto)\nVALUES\n` +
             rows.reduce<string[]>((acc, row) => {
                 if ([...requiredFields].every(field => (row)[field] !== undefined)) {
-                    if (!row.num_camisa) row.num_camisa = 'NULL';
-                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", "${row.cartao}", "${row.atleta}", ${row.num_camisa}, "${row.posicao}", "${row.minuto}")`);
+                    row.num_camisa = !row.num_camisa ? 'NULL' : `${row.num_camisa}`;
+                    row.posicao = !row.posicao ? 'NULL' : `"${row.posicao}"`;
+
+                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", "${row.cartao}", "${row.atleta}", ${row.num_camisa}, ${row.posicao}, "${row.minuto}")`);
                 }
                 return acc;
             }, []).join(',\n') + ';';
@@ -89,7 +98,12 @@ export class SQLConstructor {
         sql += `INSERT INTO estatisticas (id_estatistica, id_partida, rodada, clube, chutes, chutes_a_gol, posse_de_bola, passes, precisao_passes, faltas, cartao_amarelo, cartao_vermelho, impedimentos, escanteios)\nVALUES\n` +
             rows.reduce<string[]>((acc, row) => {
                 if ([...requiredFields].every(field => (row)[field] !== undefined)) {
-                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", ${row.chutes}, ${row.chutes_no_alvo}, "${row.posse_de_bola}", ${row.passes}, "${row.precisao_passes}", ${row.faltas}, ${row.cartao_amarelo}, ${row.cartao_vermelho}, ${row.impedimentos}, ${row.escanteios})`);
+                    row.posse_de_bola = !row.posse_de_bola ? 'NULL' : `"${row.posse_de_bola}"`;
+                    row.precisao_passes = !row.precisao_passes ? 'NULL' : `"${row.precisao_passes}"`;
+                    row.posse_de_bola = row.posse_de_bola == '\"None\"' ? 'NULL' : `${row.posse_de_bola}`;
+                    row.precisao_passes = row.precisao_passes == '\"None\"' ? 'NULL' : `${row.precisao_passes}`;
+
+                    acc.push(`\t(NULL, ${row.partida_id}, ${row.rodata}, "${row.clube}", ${row.chutes}, ${row.chutes_no_alvo}, ${row.posse_de_bola}, ${row.passes}, ${row.precisao_passes}, ${row.faltas}, ${row.cartao_amarelo}, ${row.cartao_vermelho}, ${row.impedimentos}, ${row.escanteios})`);
                 }
                 return acc;
             }, []).join(',\n') + ';';
