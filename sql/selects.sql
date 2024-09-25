6f41d83e8874a88506620c56e3fb53871818cefd
itsryu <joaovictor9162@outlook.com>
2024-09-25 20:03:17 -0300

    fix: fixing selects

--- sql/selects.sql
+++ sql/selects.sql
@@ -2,5 +2,3 @@ SELECT * FROM cartoes WHERE clube ='Flamengo';
 SELECT * FROM gols WHERE clube = 'Flamengo' AND atleta = 'Alecsandro';
 SELECT * FROM partidas WHERE rodada = 38;
 SELECT * FROM gols WHERE tipo_de_gol ='Gol Contra';
-
-SELECT tipo_de_gol FROM gols WHERE;
